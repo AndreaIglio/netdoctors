65a1bde27639a0d99135c34704453d809f5f9c02
Tommaso Figlia <tommasofiglia@gmail.com>
2021-03-12 17:47:26 +0100

    Start to make navbar

---
+++ resources/views/layouts/guest/partials/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav id="navbar_guest">
+  <ul class="d_flex">
+    <li><a href="#">prova</a></li>
+    <li><a href="#">prova</a></li>
+    <li><a href="#">prova</a></li>
+    <li><a href="#">prova</a></li>
+  </ul>
+
+  <div class="row">
+
+    <div class="col-lg-6 col-md-12">
+      <form action="index.html" method="post" class="d_flex">
+        <input type="text" name="search" value="" placeholder="Search for doctors">
+        <button type="submit" name="button">GO</button>
+      </form>
+    </div>
+
+    <div class="col-lg-6">
+      <img src="{{asset('img/Medical.png')}}" alt="">
+    </div>
+  </div>
+
+</nav>
