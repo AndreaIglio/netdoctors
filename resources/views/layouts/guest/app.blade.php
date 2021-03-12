c1f74cb83fc956d9a1a6fec2704600ba853ba0c3
Romina Trazzi <romina.trazzi@posta.it>
2021-03-12 14:22:02 +0100

    Fix color-map

--- resources/views/layouts/guest/app.blade.php
+++ resources/views/layouts/guest/app.blade.php
@@ -16,6 +16,7 @@
 
     <!-- Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
